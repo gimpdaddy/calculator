5b1e472e96e7432394a119220d48530c27bd7baa
Will Smith <williamrandellsmith@gmail.com>
2023-08-16 10:55:38 +1200

    fixed +/- exceptions and decimals

--- function.js
+++ function.js
@@ -9,11 +9,25 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 // const divide = (a, b) => a / b;
 
+function neg1() {
+    if (bString === 0 | bString === "") {
+        bString = "-";
+    } else {
+        bString = `${bString * -1}`;
+    }
+    displayText.textContent = bString;
+}
+
+function percentage() {
+    bString = bString / 100;
+    displayText.textContent = bString;
+}
+
 function divide(a, b) {
     if (b === 0) {
         return "error /0";
     } else {
-        return a / b;
+        return (a / b).toFixed(2);
     }
 }
 
@@ -41,7 +55,7 @@ function displayBstring(e) {
 
 function shiftVars() {
     a = b;
-    b = parseInt(bString);
+    b = Number(bString);
     bString = "";
 }
 
@@ -104,14 +118,20 @@ const btnAdd = document.getElementById("add");
 btnAdd.addEventListener("click", chainResult);
 
 const btnEqual = document.getElementById("equal");
-btnEqual.addEventListener("click", displayResult)
+btnEqual.addEventListener("click", displayResult);
 
 const btnNumbers = document.querySelectorAll(".nums");
 btnNumbers.forEach(el => el.addEventListener("click", displayBstring));
+
+const btnNeg1 = document.getElementById("neg1");
+btnNeg1.addEventListener("click", neg1);
+
+const btnPercentage = document.getElementById("percentage");
+btnPercentage.addEventListener("click", percentage);
 
 const displayText = document.querySelector(".display");
 
 //to add in:
 
 //decimal places
-//handle divide 0
+//work with non integers
